4bdc628d825cfb5db6a3f50001e765a3445d62bf
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-05-24 11:27:24 -0500

    number variable example

--- src/main/java/com/codeup/spring/controllers/HelloController.java
+++ src/main/java/com/codeup/spring/controllers/HelloController.java
@@ -1,9 +1,7 @@
 package com.codeup.spring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -13,9 +11,16 @@ public class HelloController {
     public String hello() {
         return "Hello from Spring!";
     }
+//    Path variable used to add input to a URI request
     @GetMapping("/hello/{name}")
     @ResponseBody
     public String sayHello(@PathVariable String name) {
         return "Hello " + name + "!";
+    }
+//    Create a path variable that takes a number instead of a string.
+    @RequestMapping(path= "/increment/{number}", method = RequestMethod.GET)
+    @ResponseBody
+    public String addOne(@PathVariable int number) {
+        return number + " plus one is " + (number + 1) + ".";
     }
 }

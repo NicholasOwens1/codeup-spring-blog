fef683874e321c7c72c097242f7d7e20a4d9364f
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-05-31 14:41:07 -0500

    PostController set up

--- src/main/java/com/codeup/spring/controllers/PostController.java
+++ src/main/java/com/codeup/spring/controllers/PostController.java
@@ -4,33 +4,36 @@ import ch.qos.logback.core.model.Model;
 import com.codeup.spring.models.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PostController {
+    @GetMapping("/posts")
+    @ResponseBody
+    public String index(){
+        return "posts index page";
+    }
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String show(){
+        return "view an individual post";
+    }
     @GetMapping("/posts/create")
-//    @ResponseBody
+    @ResponseBody
     public String showPosts(Model model){
         return "posts/create";
     }
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String createPost(){
+        return "create a new post";
+    }
+
     private final PostRepository postDao;
 
     public PostController(PostRepository postDao) {
         this.postDao = postDao;
-    }
-
-    @ResponseBody
-    public String showOnePost(){
-            return "view an individual post";
-        }
-
-    @ResponseBody
-    public String showCreateForm(){
-            return "view the form for creating a post";
-        }
-    @ResponseBody
-    public String createPost(){
-            return "create a new post";
     }
 
 }
